e42c5c7c81afeb92f86265c14549dbaf65839fec
Laurie Crean <lmcrean@gmail.com>
2024-02-15 21:29:45 +0000

    Add comments and update element selector in entry.js and index.html

--- docs/entry.js
+++ docs/entry.js
@@ -1,5 +1,7 @@
+// docs/entry.js
+
 import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 
-render(<App />, document.querySelector("#app"));
+render(<App />, document.querySelector("#root"));
